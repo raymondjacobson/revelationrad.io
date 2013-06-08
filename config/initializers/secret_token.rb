9340614f5500cf92dfc1e315a576f31d0099f640
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-06-08 12:08:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Revelradio::Application.config.secret_token = 'c3f71d4b7430a5a154faf43821a0fdb8bf7b9441f223a748d692c02da8c1bf5e9901a5f2b6c6cd009552b5c3b9c1fd7cbdf583ea3152af3854ba94521d496919'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Revelradio::Application.config.session_store :cookie_store, :key => '_revelradio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Revelradio::Application.config.session_store :active_record_store
